13ce57ce21f66a60ab839d2030fa7c25c4c19f8d
rebishop <ebishop@foundationwealth.com>
2026-04-16 19:52:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ h1, h2, h3 {
 /* Buttons */
 .stButton button {
     background-color: #7f8c6b;
-    color: white;
+    color: dark gray;
     border-radius: 10px;
     font-weight: 600;
     border: none;
